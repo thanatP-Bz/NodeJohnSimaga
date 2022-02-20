1c3b69532e652d2d18d36dd2a3d3491d826e7f76
Thanat <bztzn512@gmail.com>
2022-02-20 18:36:04 -0500

    http

--- async.js
+++ async.js
@@ -6,3 +6,18 @@ readFile("./content/first.txt", "utf8", (err, result) => {
   }
   console.log(result);
 });
+
+const { readFile, writeFile } = require("fs").promises;
+
+const start = async () => {
+  try {
+    const first = await readFile("./content/first.txt", "utf-8");
+    //write file do not need to put in the variable
+    await writeFile("./content/second.txt", "This is the second text"); //We can apprend by flag: ("a")
+    console.log(first);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
