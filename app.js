150d833cd44bbdb30baea30032f3ebc00e971ee1
Thanat <bztzn512@gmail.com>
2022-02-19 21:53:41 -0500

    nodeAsync

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("hello node");
+const http = require("http");
+
+const sever = http.createServer((req, res) => {
+  console.log("request event");
+  res.end("hello world");
+});
+
+sever.listen(5000, () => {
+  console.log("listen to the port 5000");
+});
